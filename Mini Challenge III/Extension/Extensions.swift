d123ff5265956418b09f27843e5f8d01e2f52222
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-04-23 08:22:06 -0300

    Add view and controllers have resposible to page

---
+++ Mini Challenge III/Extension/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  Mini Challenge III
+//
+//  Created by Vinicius Mangueira Correia on 22/04/19.
+//  Copyright © 2019 Vinicius Mangueira Correia. All rights reserved.
+//
+
+import Foundation
